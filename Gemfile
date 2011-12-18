9ffa806cf99ac4d5b30eaf43ef5b32fdb7e81d2d
Morgan Brown <brown.mhg@gmail.com>
2011-12-18 13:01:34 -0800

    initial

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'thin'
+
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', '~> 0.8.3', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MorganIo::Application.config.secret_token = '56e0c8946969df050bcfbcb5656e8c53b1b7d25c55ee7af6620d861e1221ac0fb4e92e6b4e8abf5a8ee5125d22a96cb3f0489ca0af3a27afdbb1689d8db2c61b'
