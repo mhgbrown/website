d1428fb3e83a4a4d194908a7ef89503ccfd340fe
Morgan Brown <brown.mhg@gmail.com>
2013-05-21 18:12:57 -0700

    Some small cleanup
    * Reformat jquery.extensions.js
    * Put the right mime type on my favicon

--- app/assets/javascripts/jquery.extensions.js
+++ app/assets/javascripts/jquery.extensions.js
@@ -1,39 +1,33 @@
-(function ($){
+(function ( $ ) {
 
   // Set a selection range with the given start and end indicies
-  $.fn.setSelection = function(start, end){
-    this.each(function(index, element){
-      if(typeof element.setSelectionRange === "function"){
-        element.setSelectionRange(start, end);
-      }else if(typeof element.createTextRange === "function"){
+  $.fn.setSelection = function( start, end ) {
+    return this.each(function( index, element ) {
+      if( typeof element.setSelectionRange === "function" ) {
+        element.setSelectionRange( start, end );
+      }else if( typeof element.createTextRange === "function" ) {
         range = element.createTextRange();
-          range.collapse(true);
-          range.moveEnd('character', start);
-          range.moveStart('character', end);
-          range.select();
+        range.collapse( true );
+        range.moveEnd( 'character', start );
+        range.moveStart( 'character', end );
+        range.select();
       }
     });
-
-    return this;
   };
 
   // Set the position of the cursor to the given index
-  $.fn.setCaretPosition = function(pos){
-    this.each(function(index, element){
-      $(element).setSelection(pos, pos);
+  $.fn.setCaretPosition = function( pos ) {
+    return this.each(function( index, element ) {
+      $(element).setSelection( pos, pos );
     });
-
-    return this;
   };
 
   // Set the position of the cursor to the end
-  $.fn.setCaretAtEnd = function(){
-    this.each(function(index, element){
+  $.fn.setCaretAtEnd = function() {
+    return this.each(function( index, element ) {
       var length = element.value.length;
-      $(element).setCaretPosition(length, length);
+      $(element).setCaretPosition( length, length );
     });
-
-    return this;
   };
 
-})(jQuery)
+})( jQuery );
