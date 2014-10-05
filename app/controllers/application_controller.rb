4dd82a8c83ec7a5e09b6367b1f7a7cea48a60657
Morgan Brown <brown.mhg@gmail.com>
2014-10-05 17:30:42 -0400

    Add edition to website
    * Add edition as 7 character aha link to github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
+    @sha = `git rev-parse HEAD`
+    @short_sha = `git rev-parse --short=7 HEAD`
   end
 
 end
