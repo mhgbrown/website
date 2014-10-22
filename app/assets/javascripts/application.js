29800679d795f0234cfcd25d0147149ebf186a10
Morgan Brown <brown.mhg@gmail.com>
2014-10-22 22:40:16 +0200

    Hi, New Design
    * Updated javascript URLs to work in FireFox
    * Add randomColor js lib
    * Add mobile zooming

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,43 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require_tree .
+
+$(function() {
+
+  var $contentLeft = $('.content-left'),
+    $contentRight = $('.content-right');
+
+  function cycleColor() {
+    var color = randomColor({ luminosity: 'bright' });
+    $contentLeft.css('background-color', color);
+    $contentRight.css('color', color);
+
+    $(document)
+      .off('mouseenter mouseleave', '.navigation.right a')
+      .on({
+        mouseenter: function (event) {
+          var $target = $(event.target);
+          $target.css('background-color', $contentLeft.css('background-color'));
+        },
+        mouseleave: function (event) {
+          var $target = $(event.target);
+          $target.css('background-color', 'inherit');
+        }
+      }, '.navigation.right a')
+      .off('mouseenter mouseleave', '.navigation.left a')
+      .on({
+        mouseenter: function (event) {
+          var $target = $(event.target);
+          $target.css('color', $contentLeft.css('background-color'));
+        },
+        mouseleave: function (event) {
+          var $target = $(event.target);
+          $target.css('color', 'inherit');
+        }
+      }, '.navigation.left a');
+
+  }
+
+  setInterval(cycleColor, 5000);
+
+});
